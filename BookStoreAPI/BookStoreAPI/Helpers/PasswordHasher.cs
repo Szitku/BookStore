19e122c72063061160ac66c682f0bf05b1aeb6ac
Szita Gábor <szitagabormatek@gmail.com>
2023-06-30 13:45:49 +0200

    PasswordHasher class added to the API

---
+++ BookStoreAPI/BookStoreAPI/Helpers/PasswordHasher.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+
+namespace BookStoreAPI.Helpers
+{
+    public class PasswordHasher
+    {
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+        private static readonly int SaltSize = 16;
+        private static readonly int HashSize = 20   ;
+        private static readonly int Iterations = 10000;
+
+        public static string HashPassword(string password) 
+        {
+            byte[] salt = new byte[SaltSize];
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password,salt,Iterations);
+            byte[] hash = key.GetBytes(HashSize);
+
+            byte[] hashbytes = new byte[SaltSize + HashSize];
+            Array.Copy(salt, hashbytes, SaltSize);
+            Array.Copy(hash,0,hashbytes,SaltSize,HashSize);
+
+            return Convert.ToBase64String(hashbytes);
+        }
+
+        public static bool VerifyPassword(string password, string base64hash) 
+        {
+            byte[] hashBytes = Convert.FromBase64String(base64hash);
+            byte[] salt = new byte[SaltSize];
+
+            Array.Copy(hashBytes, salt, SaltSize);
+
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password,salt,Iterations);
+            byte[] hash = key.GetBytes(HashSize);
+
+            for (int i = 0; i < HashSize; i++)
+            {
+                if (hashBytes[i + SaltSize] != hash[i]) 
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+    }
+}
