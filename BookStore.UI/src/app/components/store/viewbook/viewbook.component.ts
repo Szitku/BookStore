e26ca22c43afd984b5d2e953029ab274b462e979
Szita Gábor <szitagabormatek@gmail.com>
2023-07-28 12:49:07 +0200

    viewbook page implemented with other minor design changes

--- BookStore.UI/src/app/components/store/viewbook/viewbook.component.ts
+++ BookStore.UI/src/app/components/store/viewbook/viewbook.component.ts
@@ -10,7 +10,8 @@ import { BooksService } from 'src/app/services/books.service';
 })
 export class ViewbookComponent implements OnInit {
 
-  book : Book = {} as Book;  
+  book : Book = {} as Book;
+  bookfound : boolean = false;
   constructor(private BookService : BooksService, private route : ActivatedRoute){}
   
   ngOnInit(): void {
@@ -21,16 +22,21 @@ export class ViewbookComponent implements OnInit {
           this.BookService.getBookbyId(id).subscribe({
             next : (res) => {
               this.book = res;
+              this.bookfound = true;
               console.log(this.book);
             },
             error : (err) => {
+              this.bookfound = false;
               console.log(err.error.message);
             }
           })
         }
       }
     })
-    
+  }
+
+  addCart(book : Book) : void {
+
   }
 
 }
