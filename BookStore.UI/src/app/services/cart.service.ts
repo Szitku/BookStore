ac617fd1e7cefe8ca7ec8a4ce5ca8b82de8b5e96
Szita Gábor <szitagabormatek@gmail.com>
2023-08-01 14:04:05 +0200

    cartservice - addorder and getorder

--- BookStore.UI/src/app/services/cart.service.ts
+++ BookStore.UI/src/app/services/cart.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../models/bookmodel';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  private orders$ = new BehaviorSubject<Map<Book,number>>(new Map<Book,number>());
 
   constructor() { }
+
+  public getOrders() : Observable<Map<Book,number>>{
+    return this.orders$.asObservable();
+  }
+
+  public addOrder(book : Book,amount : number) : void{
+    let orders = this.orders$.getValue();
+    if(!orders.has(book)){
+      orders.set(book,amount);
+    }else{
+      let old = orders.get(book) ?? 0;
+      orders.set(book,old + amount);
+    }
+    this.orders$.next(orders);
+  }
+
 }
