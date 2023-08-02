ff34ebbad0b23550f38812d2bae2e7c5f3601989
Szita Gábor <szitagabormatek@gmail.com>
2023-08-02 11:54:16 +0200

    cart page +/-/delte of orders is working

--- BookStore.UI/src/app/components/store/cart/cart.component.ts
+++ BookStore.UI/src/app/components/store/cart/cart.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/bookmodel';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+
+  orders = new Map<Book,number>()
+
+  constructor(private cart : CartService) {}
+
+  ngOnInit(): void {
+    this.cart.getOrders().subscribe({next : (ordermap) => {
+      if(ordermap){
+        this.orders = ordermap;
+      }
+    }})
+  }
+
+  addOneOrderAmount(book : Book):void{
+    this.cart.addOrder(book,1);
+  }
+
+  removeOneOrderAmount(book : Book):void {
+    this.cart.removeAmountOrder(book);
+  }
+
+  deleteOrder(book : Book) : void {
+    this.cart.deleteOrder(book);
+  }
+
+  mapToArray(map: Map<Book, number>) {
+    return Array.from(map.entries()).map(([key, value]) => ({ key, value }));
+  }
+
 
 }
